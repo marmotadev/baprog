27569832453f9cfb07a088fb312584f6b1fd8a5c
beabras <jaunius@gmail.com>
2013-12-04 09:25:59 +0200

    initial

---
+++ src/main/java/ba/EntryPoint.java
@@ -0,0 +1,9 @@
+package ba;
+import java.io.IOException;
+
+public class EntryPoint {
+    public static void main(String[] args) throws IOException {
+        InputHandler ih = new InputHandler();
+        ih.handleEvents();
+    }
+}
---
+++ src/main/java/ba/InputHandler.java
@@ -0,0 +1,42 @@
+package ba;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class InputHandler {
+
+	public void handleEvents() {
+		System.out.println("Starting ");
+		Console con;
+
+	    con = System.console();
+
+	    if (con == null)
+	        throw new RuntimeException("no console");
+	    
+	    while (true) {
+	    	String str = con.readLine(">");
+	    	Scanner lineTokenizer = new Scanner(str);
+	    	if (lineTokenizer.hasNext()) 
+	    	{
+		    	  String token = lineTokenizer.next();  // consume the valid token
+		    	  System.out.println("Token " + token);
+		      }
+	    	con.printf("Here is your string %s", str);
+	    }
+	    
+//	    if(true) return;
+//		
+//		
+//		Scanner sc = new Scanner(System.in);
+//		 while (sc.hasNextLine()) {
+//			 Scanner lineTokenizer = new Scanner(sc.nextLine());
+//		      
+//		      if (lineTokenizer.hasNext()) {
+//		    	  String token = lineTokenizer.next();  // consume the valid token
+//		    	  System.out.println("Token " + token);
+//		      }
+//		}
+	}
+
+}
