9f3067e765686701b0bf84d0197ebf82da91c2be
beabras <jaunius@gmail.com>
2013-12-04 10:20:25 +0200

    framework

--- src/main/java/ba/InputHandler.java
+++ src/main/java/ba/InputHandler.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class InputHandler {
 
 	private Logic logic;
@@ -18,81 +17,126 @@ public class InputHandler {
 		System.out.println("Starting ");
 		Console con;
 
-	    con = System.console();
+		con = System.console();
 
-	    if (con == null)
-	        throw new RuntimeException("no console");
-	    
-	    while (true) {
-	    	String str = con.readLine(">");
-	    	Scanner lineTokenizer = new Scanner(str);
-	    	String cmd = null;
-	    	List<String> params = new ArrayList<String>();
-	    	while (lineTokenizer.hasNext()) 
-	    	{
-		    	  String token = lineTokenizer.next();  // consume the valid token
-		    	  if (cmd == null)
-		    		  cmd = token;
-		    	  else
-		    		  params.add(token);
-		    	  System.out.println("Token " + token);
-		    }
-//	    	con.printf("Here is your string %s", str);
-	    	handleCommand(cmd, params);
-	    	cmd = null;
-	    }
-	    
-//	    if(true) return;
-//		
-//		
-//		Scanner sc = new Scanner(System.in);
-//		 while (sc.hasNextLine()) {
-//			 Scanner lineTokenizer = new Scanner(sc.nextLine());
-//		      
-//		      if (lineTokenizer.hasNext()) {
-//		    	  String token = lineTokenizer.next();  // consume the valid token
-//		    	  System.out.println("Token " + token);
-//		      }
-//		}
+		if (con == null)
+			throw new RuntimeException("no console");
+
+		while (true) {
+			String str = con.readLine(">");
+			Scanner lineTokenizer = new Scanner(str);
+			String cmd = null;
+			List<String> params = new ArrayList<String>();
+			while (lineTokenizer.hasNext()) {
+				String token = lineTokenizer.next(); // consume the valid token
+				if (cmd == null)
+					cmd = token;
+				else
+					params.add(token);
+//				System.out.println("Token " + token);
+			}
+			// con.printf("Here is your string %s", str);
+			try {
+				handleCommand(cmd, params);
+			}
+			catch (IllegalArgumentException e)
+			{
+				System.err.println("Illegal command provided:" + e.getMessage());
+			}
+			catch (Exception e) {
+				System.err.println("Fucked up, try again");
+				e.printStackTrace();
+			}
+			cmd = null;
+		}
+
+		// if(true) return;
+		//
+		//
+		// Scanner sc = new Scanner(System.in);
+		// while (sc.hasNextLine()) {
+		// Scanner lineTokenizer = new Scanner(sc.nextLine());
+		//
+		// if (lineTokenizer.hasNext()) {
+		// String token = lineTokenizer.next(); // consume the valid token
+		// System.out.println("Token " + token);
+		// }
+		// }
 	}
 
 	private void handleCommand(String cmd, List<String> params) {
-		switch (cmd) {
-			case "exit":
-				System.exit(0);
-			break;
-			case "placeships":
-				noParamsExpect(params);
-				break;
-			case "theymoved":
-				break;
-			case "theyshot":
-				//<koordinate>
-				break;
-			case "last":
-				//<hit|empty|dead>
-				break;
-			case "shoot":
-
-				break;
-			case "print":
-
-				break;
-			case "undo":
-				//
-				break;
-				
+		if (cmd == null) {
+			return;
 		}
-		
+		switch (cmd) {
+		case "exit":
+			System.exit(0);
+			break;
+		case "placeships":
+			noParamsExpect(params);
+			logic.placeShips();
+			break;
+		case "theymoved":
+			logic.theyMoved();
+			break;
+		case "theyshot":
+			// <koordinate>
+			expectParams(params, 2, "syntax: theyshot E 5");
+			Coordinate c = parseCoords(params);
+			logic.theyShot(c);
+			break;
+		case "last":
+			expectParams(params, 1, "syntax: last  <hit|empty|dead>");
+			ActionSymbols s = parseSymbol(params.get(0));
+			logic.lastMoveFromEnemy(s);
+			// <hit|empty|dead>
+			break;
+		case "shoot":
+			logic.shoot();
+			break;
+		case "print":
+			logic.print();
+			break;
+		case "undo":
+			logic.undo();
+			break;
+		default:
+			System.err.println("Did not understand command");
+		}
+			
+
 	}
 
-	private boolean noParamsExpect(List<String> params) {
-		if (params.size() > 0){
-			System.out.println("No parameters expected! Reenter command!");
-			return false;
+	private ActionSymbols parseSymbol(String string) {
+		switch (string) {
+		case "hit":
+			return ActionSymbols.WOUNDED;
+
+		case "empty":
+			return ActionSymbols.SEA;
+
+		case "dead":
+			return ActionSymbols.SHOT;
+		default:
+			throw new IllegalArgumentException(string);
 		}
-		return true;
-		
+	}
+
+	private Coordinate parseCoords(List<String> params) {
+		return new Coordinate(params.get(0), params.get(1));
+	}
+
+	private void expectParams(List<String> params, int i, String msg) {
+		if (params.size() != i) {
+			throw new IllegalArgumentException("Expected param count: " + i + ", " + msg);
+		}
+	}
+
+	private void noParamsExpect(List<String> params) {
+		if (params.size() > 0) {
+			throw new IllegalArgumentException("No parameters expected! Reenter command!");
+		}
+
 	}
 
 }
--- src/main/java/ba/Logic.java
+++ src/main/java/ba/Logic.java
@@ -2,4 +2,52 @@ package ba;
 
 public class Logic {
 
+	public void placeShips() {
+		System.out.println("placing ships");
+		notImplemented();
+
+	}
+
+	public static void notImplemented() {
+		System.out.println("not implemented");
+	}
+
+	public void theyShot(Coordinate c) {
+		System.out.println("They shot at " + c);
+		notImplemented();
+	}
+
+	public void theyMoved() {
+		System.out.println("They moved");
+		notImplemented();
+	}
+
+	public void lastMoveFromEnemy(ActionSymbols s) {
+		System.out.println("Our last action resulted:" + s);
+		switch (s) {
+		case DEAD:
+			break;
+		case SEA:
+			break;
+		case WOUNDED:
+			break;
+		}
+		notImplemented();
+
+	}
+
+	public void shoot() {
+		System.out.println("Now we shoot");
+		notImplemented();
+	}
+
+	public void print() {
+		notImplemented();
+	}
+
+	public void undo() {
+		System.out.println("undoing last move");
+		notImplemented();
+	}
+
 }
