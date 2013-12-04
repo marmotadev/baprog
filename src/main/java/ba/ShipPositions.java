82c9a98c7502aa1282e717194891d1c971ee398a
smackevich <s.mackevich@baltic-amadeus.lt>
2013-12-04 11:39:27 +0200

    ShipPositions

--- src/main/java/ba/ShipPositions.java
+++ src/main/java/ba/ShipPositions.java
@@ -1,30 +1,14 @@
 package ba;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 public class ShipPositions {
-	public static LinkedHashMap<Integer, String> getPositions(){
-		LinkedHashMap<Integer,String> pos = new LinkedHashMap<>();
-		int nr = 1;
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
-		pos.put(nr++, "");
 
-		System.out.println("Position returned = "+pos);
-		return pos;
-	}
 	private static char m = ActionSymbols.SEA.getSymbol();
 	private static char k = ActionSymbols.SHIP.getSymbol();
 	public static final String map1Code = "3c1z,4a2z,1h2,2c5z,1f5,3a7z,1i7z,2cz,1g9";
-	public static final String map2Code = "";
-	public static final String map3Code = "";
-	public static final String map4Code = "";
+	public static final String map2Code = "3h1d,1a2,1d2,3j3z,1g5,1c6,,2j7z,4a10d,2f10d,2i10d";
+	public static final String map3Code = "1d1,1j2,1e4,1h6,3a8d,3e8d,2i8d,4a10d,2f10d,2i10d";
+	public static final String map4Code = "1f1,1e4,1h5,1a6,3a8d,3e8d,2i8d,4a10d,2f10d,2i10d";
 
 	public static char[][]map1={
 	//   a b c d e f g h i j
@@ -99,7 +83,7 @@ public class ShipPositions {
 
 
 	public static void main(String[] args) {
-		char[][] m = map1;
+		char[][] m = map4;
 		System.out.println("ABCDEFGHIJ");
 		for (int i = 0; i < m.length; i++) {
 			for (int ii = 0; ii < m[i].length; ii++) {
@@ -108,6 +92,5 @@ public class ShipPositions {
 			System.out.println(""+(i+1));
 		}
 		System.out.println("ABCDEFGHIJ");
-
 	}
 }
